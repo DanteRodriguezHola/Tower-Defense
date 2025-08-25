424ab69bdfcb27a832aab83ebdb9b46fd495c5ff
DanteRodriguezHola <rodriguezmedrano@alumno.huergo.edu.ar>
2025-08-25 08:20:40 -0300

    Funcionamiento básico del programa.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print("hola")
+import pygame
 
-import random
+pygame.init()
+
+#Definiendo nuestras variables
+ventana = (600, 900)
+clock = pygame.time.Clock()
+titulo = pygame.display.set_caption("Stray Tower Defense")
+jugando = True
+
+#Loop de juego básico
+while jugando:
+    pygame.display.set_mode(ventana)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           jugando = False
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
