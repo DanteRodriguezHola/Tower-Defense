c955e76744f122a5cb621faf5601a951d4714914
NG <>
2025-10-08 10:38:20 -0300

    Update tienda.py

--- tienda.py
+++ tienda.py
@@ -2,5 +2,5 @@ import config as c
 import pygame as pg
 
 def cargar_tienda():
-    tienda_imagen = pg.image.load("assets/imagenes/tienda/tienda_3.png").convert_alpha()
+    tienda_imagen = pg.image.load("assets/imagenes/tienda/tienda.png").convert_alpha()
     c.ventana.blit(tienda_imagen, (c.ancho_mapa, 0))
