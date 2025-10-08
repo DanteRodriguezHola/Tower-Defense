07db46dc410a22defb0a4dce6f506e22692a4985
luulmikuu <sbudani@alumno.huergo.edu.ar>
2025-10-08 19:26:36 -0300

    agregar hover al boton tanque

--- botones.py
+++ botones.py
@@ -1,8 +1,9 @@
 import pygame as pg
 
 class Button(pg.sprite.Sprite):
-    def __init__(self, imagen, x , y, single_click):
+    def __init__(self, imagen, imagen_hover, x , y, single_click):
         self.image = imagen
+        self.image_hov = imagen_hover
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
@@ -12,15 +13,18 @@ class Button(pg.sprite.Sprite):
         posicion_mouse = pg.mouse.get_pos()
 
         if self.rect.collidepoint(posicion_mouse):
+            # CAPAZ ESTOY MAL, HAY Q VER---------
+            surface.blit(self.image_hov, self.rect)
+            # mi proceso-------------
             if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 accion = True
 
                 if self.single_click:
                     self.clicked = True
+        else:
+            surface.blit(self.image, self.rect)
 
         if pg.mouse.get_pressed()[0] == 0:
             self.clicked = False
-
-        surface.blit(self.image, self.rect)
-
+            
         return accion
--- main.py
+++ main.py
@@ -1,65 +1,65 @@
 import config as c
-
 from rondas import procesar_ronda
 from torretas import crear_torreta, seleccionar_torreta
 from tienda import cargar_tienda
 from botones import Button
 import pygame as pg
 
-
-
-#Carga de los botones
+# Carga de los botones
 imagen_boton_tanque = pg.image.load("assets/imagenes/tienda/boton_tanque.png").convert_alpha()
-boton_tanque = Button(imagen_boton_tanque, (c.ancho_mapa + c.ancho_tienda / 19), 180, True)
+imagen_boton_tanque_hover = pg.image.load("assets/imagenes/tienda/boton_tanque_hover.png").convert_alpha()
+boton_tanque = Button(imagen_boton_tanque, imagen_boton_tanque_hover, (c.ancho_mapa + c.ancho_tienda / 19), 180, True)
 
 grupo_torretas = pg.sprite.Group()
 subrondas = procesar_ronda("1")
 
-#Variables que controlan el juego:
+grupo_enemigos = pg.sprite.Group()
+for subronda in subrondas:
+    grupo_enemigos.add(subronda)
+
+# Variables que controlan el juego:
 jugando = True
 creando_torretas = False
 torreta_seleccionada = None
 
 while jugando:
-    c.clock.tick(60) 
+    c.clock.tick(60)
     c.world.draw(c.ventana)
     cargar_tienda()
-    for subronda in subrondas:
-        grupo_enemigos = pg.sprite.Group()
-        grupo_enemigos.add(subronda)
-        #Actualizacion de los grupos
-        for enemigo in grupo_enemigos:
-            enemigo.update()
-            enemigo.draw(c.ventana)
 
-        grupo_torretas.update(grupo_enemigos)
+    # Actualización y dibujo de enemigos
+    grupo_enemigos.update()
+    for enemigo in grupo_enemigos:
+        enemigo.draw(c.ventana)
 
-        if boton_tanque.draw(c.ventana):
-            creando_torretas = True
+    # Actualización y dibujo de torretas
+    grupo_torretas.update(grupo_enemigos)
 
-        grupo_torretas.draw(c.ventana)
-        for torreta in grupo_torretas:
-            torreta.draw(c.ventana)
+    if boton_tanque.draw(c.ventana):
+        creando_torretas = True
 
-        for evento in pg.event.get():
-            #Al hacer click izquierdo
-            if evento.type == pg.MOUSEBUTTONDOWN and evento.button == 1:
-                posicion_mouse = pg.mouse.get_pos() #Se obtiene la posicion del ratón
-                
-                #Si se a pulsado el boton de la torreta
-                if creando_torretas:
-                    #Si esta dentro del mapa, se crea una torreta
-                    if posicion_mouse[0] < c.ancho_mapa and posicion_mouse[1] < c.alto_mapa:
-                        crear_torreta(posicion_mouse, grupo_torretas)
-                    else:
-                        torreta_seleccionada = seleccionar_torreta(posicion_mouse, grupo_torretas)
-                
-            #Salir del programa
-            if evento.type == pg.QUIT:
-                jugando = False
-        pg.display.flip()
+    grupo_torretas.draw(c.ventana)
+    for torreta in grupo_torretas:
+        torreta.draw(c.ventana)
+
+    for evento in pg.event.get():
+        #Al hacer click izquierdo
+        if evento.type == pg.MOUSEBUTTONDOWN and evento.button == 1:
+            posicion_mouse = pg.mouse.get_pos() #Se obtiene la posicion del ratón
             
-           
+            #Si se a pulsado el boton de la torreta
+            if creando_torretas:
+                #Si esta dentro del mapa, se crea una torreta
+                if posicion_mouse[0] < c.ancho_mapa and posicion_mouse[1] < c.alto_mapa:
+                    crear_torreta(posicion_mouse, grupo_torretas)
+                else:
+                    torreta_seleccionada = seleccionar_torreta(posicion_mouse, grupo_torretas)
+        #Salir del programa
+        if evento.type == pg.QUIT:
+            jugando = False
+
+    pg.display.flip()
+
 pg.quit()
 
 if __name__ == "__main__":
