6ddef11a2d3b1464a35474d493a1e487508a2697
sbudani-cloud <sbudani@alumno.huergo.edu.ar>
2025-11-10 10:34:15 -0300

    QUEE soy una genia sjdakro3q
    podes ver la vida de los zo,mbies en la esquina d la pantalals djsdkj

--- main.py
+++ main.py
@@ -188,6 +188,9 @@ while jugando:
         if evento.type == pg.KEYDOWN:
             tecla_presionada = evento.key
             print(tecla_presionada)
+    mouse_pos = pg.mouse.get_pos()
+    for enemigo in grupo_enemigos:
+        enemigo.ver_info(mouse_pos, c.ventana)
 
     pg.display.flip()
 
